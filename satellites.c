e1a6e9bdadb08e1b837df63bf634b7462e586b86
DO7OO from 07/2021 on <de8msh@gmx.de>
2026-04-20 14:31:54 +0200

    Add Aero 10500 baud channels to satellites.c

--- satellites.c
+++ satellites.c
@@ -29,6 +29,9 @@ static const channel_def_t channels_whq[] = {
     { 1545122890.0, CHAN_AERO_600,  1 },
     { 1545127910.0, CHAN_AERO_1200, 2 },
     { 1545132890.0, CHAN_AERO_600,  3 },
+    /* Aero 10500 baud channels */
+    { 1546213450.0, CHAN_AERO_10500, 4 },
+    { 1546233420.0, CHAN_AERO_10500, 5 },
 };
 
 /* I4-F3 (98W, AORW) -- Americas region
